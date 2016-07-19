fc1fd22cf1531f2dc02237f559d34ae7e24932ee
kahn1990 <kangjian1990@live.com>
2016-07-19 12:55:37 +0800

    添加覆盖率测试

--- Makefile
+++ Makefile
@@ -2,9 +2,15 @@ TESTS = test/*.js
 REPORTER = spec
 TIMEOUT = 5000
 MOCHA = ./node_modules/mocha/bin/mocha
+MOCHA2 = ./node_modules/mocha/bin/_mocha
+ISTANBUL = ./node_modules/.bin/istanbul
 
 test:
 	@npm install
 	@$(MOCHA) --reporter $(REPORTER) --timeout $(TIMEOUT) $(MOCHA_OPTS) $(TESTS)
 
+istanbul:
+	@$(ISTANBUL) cover $(MOCHA2) test/jdz.js
+
 .PHONY: test
+.PHONY: istanbul
--- test/jdz.js
+++ test/jdz.js
@@ -1,12 +1,35 @@
-var exec   = require('child_process').exec;
-var expect = require('expect.js');
+var expect                = require('expect.js'),
+    path                  = require('path'),
+    jsdoc_zero            = require('../lib/default'),
+    _                     = require('underscore'),
+    projectHomePath       = path.resolve('.'),
+    defaultConfigFile     = require(path.resolve(__dirname, '../lib/default.json')),
+    userDefinedConfigFile = require(path.resolve(__dirname, '../dox.config.json')),
+    loadConfigFiled       = _.extend(defaultConfigFile, userDefinedConfigFile);
+
 
 describe('bin/dox build', function () {
-    it('build default', function (done) {
-        exec('node ' + __dirname + '/../bin/dox build', function (err, stdout) {
-            expect(err).not.to.be.ok();
-            expect(stdout).to.be.ok();
-            done();
+    /*
+     var exec                  = require('child_process').exec;
+     it('build default', function (done) {
+     exec('node ' + __dirname + '/../bin/jdz build', function (err, stdout) {
+     expect(err).not.to.be.ok();
+     expect(stdout).to.be.ok();
+     done();
+     });
+     });
+     */
+    it('test cover', function (done) {
+        if (!_.isArray(loadConfigFiled.source.include)) loadConfigFiled.source.include = defaultConfigFile.source.include;
+        if (!_.isArray(loadConfigFiled.source.exclude)) loadConfigFiled.source.exclude = defaultConfigFile.source.exclude;
+        if (!_.isString(loadConfigFiled.source.output)) loadConfigFiled.source.output = defaultConfigFile.source.output;
+        if (!_.isArray(loadConfigFiled.source.suffix)) loadConfigFiled.source.suffix = defaultConfigFile.source.suffix;
+        loadConfigFiled.source.exclude  = _.map(loadConfigFiled.source.exclude, function (item) {
+            return path.normalize(item);
         });
+        loadConfigFiled.projectHomePath = projectHomePath;
+        loadConfigFiled.source.output   = path.normalize(loadConfigFiled.source.output);
+        expect(jsdoc_zero(loadConfigFiled)).to.equal(true);
+        done();
     });
 });
